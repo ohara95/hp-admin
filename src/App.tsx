5f3d37712965eaa469556d3932b2d8e2f1404184
ohara95 <erc.fleur@gmail.com>
2020-10-19 01:42:01 +0900

    adminと分離したので不要ファイル削除

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { AuthProvider } from "./AuthProvider";
+import MainPage from "./hp/pages/MainPage";
+import "./App.css";
+
+const App = () => {
+  return (
+    <>
+      <AuthProvider>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path="/:param" component={MainPage} />
+            <Route path="/" component={MainPage} />
+          </Switch>
+        </BrowserRouter>
+      </AuthProvider>
+    </>
+  );
+};
+
+export default App;
--- src/admin/atoms/index.ts
+++
@@ -1,9 +0,0 @@
-export { default as Alert } from "./Alert";
-export { default as Button } from "./Button";
-export { default as Input } from "./Input";
-export { default as Label } from "./Label";
-export { default as Textarea } from "./Textarea";
-export { default as Select } from "./Select";
-
-export { default as IconPop } from "./IconPop";
-export { default as SwitchButton } from "./SwitchButton";
